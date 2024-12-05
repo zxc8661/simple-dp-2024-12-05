f60a7347d1c58194ea5cbad959e4fa6d60815793
janjan <97529177+zxc8661@users.noreply.github.com>
2024-12-05 12:06:15 +0900

    create SimpleDb class, r

---
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -0,0 +1,36 @@
+package com.ll.simpleDb;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class SimpleDbTest {
+
+    private static SimpleDb simpleDb;
+
+    @BeforeAll
+    public static void beforeAll() {
+        simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
+
+        createArticleTable();
+    }
+
+    private static void createArticleTable() {
+        simpleDb.run("DROP TABLE IF EXISTS article");
+
+        simpleDb.run("""
+                CREATE TABLE article (
+                    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                    PRIMARY KEY(id),
+                    createdDate DATETIME NOT NULL,
+                    modifiedDate DATETIME NOT NULL,
+                    title VARCHAR(100) NOT NULL,
+                    `body` TEXT NOT NULL,
+                    isBlind BIT(1) NOT NULL DEFAULT 0
+                )
+                """);
+    }
+}
+}
