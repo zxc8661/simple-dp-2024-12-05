2f436a118fbbcd3eb06f1b2bdaad23553e1b012a
janjan <97529177+zxc8661@users.noreply.github.com>
2024-12-05 11:46:25 +0900

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,18 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    runtimeOnly("com.mysql:mysql-connector-j:9.1.0")
+
+    testImplementation("org.assertj:assertj-core:3.26.3")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
 }
 
 tasks.test {
